af841256025579db2190890e14072689db8f145b
Chandlerzz <Chandlerzz@github.com>
2021-11-11 13:57:30 +0800

    continue

--- install.sh
+++ install.sh
@@ -5,3 +5,5 @@ ln -s $HOME/dotfile/.tmux.conf $HOME/.tmux.conf
 ln -s $HOME/dotfile/.bashrc $HOME/.bashrc
 # man link  手动执行
 sudo ln -s $HOME/dotfile/man /usr/share/man/man1c
+# .ipython Link
+ln -s $HOME/dotfile/.ipython $HOME/.ipython
