c9e560fdc38ccb4f6e811b10f79fb3ec5ff63c59
Chandlerzz <664837078@qq.com>
2021-11-19 23:40:40 -0500

    continue

--- install.sh
+++ install.sh
@@ -3,3 +3,19 @@
 sudo ln -s $HOME/dotfile/man /usr/share/man/man1c
 make ~/dotfile/script/c/rmswp
 cp ~/dotfile/script/c/rmswp ~/bin
+
+mkdir -p ~/.vim/pack/tpope/start
+cd ~/.vim/pack/tpope/start
+git clone https://tpope.io/vim/surround.git
+vim -u NONE -c "helptags surround/doc" -c q
+# mkdir -p ~/.vim/pack/tpope/start
+cd ~/.vim/pack/tpope/start
+git clone https://tpope.io/vim/commentary.git
+vim -u NONE -c "helptags commentary/doc" -c q
+# mkdir -p ~/.vim/pack/tpope/start
+cd ~/.vim/pack/tpope/start
+git clone https://tpope.io/vim/fugitive.git
+vim -u NONE -c "helptags fugitive/doc" -c q
+mkdir -p ~/.vim/pack/tpope/start
+cd ~/.vim/pack/tpope/start
+git clone https://tpope.io/vim/repeat.git
